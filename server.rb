a808417df0d64a5402d4d94dde77f65f106c5b82
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-18 20:39:07 +0000

    users can now be prompted if wrong password

--- server.rb
+++ server.rb
@@ -5,23 +5,32 @@ require './lib/link'
 require './lib/user'
 require './lib/helpers/application'
 require './lib/data_mapper_setup'
+require 'rack-flash'
 
 
 enable :sessions
 set :session_secret, 'super secret'
-
+use Rack::Flash
+use Rack::MethodOverride
 
 get '/' do
 	erb :index
 end
 
 get '/users/new' do
+  @user = User.new
   erb :"users/new"
 end
 
 post '/users' do
-  user = User.create(:email => params[:email],
-                     :password => params[:password])
-  session[:user_id] = user.id
-  redirect to('/')
+  @user = User.new(:email => params[:email], 
+              :password => params[:password],
+              :password_confirmation => params[:password_confirmation])  
+  if @user.save
+    session[:user_id] = @user.id
+    redirect to('/')
+  else
+    flash[:notice] = "Sorry, your passwords don't match"
+    erb :"users/new"
+  end
 end
