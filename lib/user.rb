54deb04d68bc818617cf8e7712a30af1bfa73701
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-18 21:05:50 +0000

    created errors for same address messages

--- lib/user.rb
+++ lib/user.rb
@@ -4,13 +4,13 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
-  property :password_digest, Text
+	property :email, String, :unique => true, :message => "This email is already taken"
+	property :password_digest, Text
 
-  attr_reader :password
+	attr_reader :password
 	attr_accessor :password_confirmation
-
-	validates_confirmation_of :password
+	validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
+	validates_uniqueness_of :email
 
   def password=(password)
     @password = password
