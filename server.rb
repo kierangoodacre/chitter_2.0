ba630aa8b91245e8aba2d0421609d5533d16cb64
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-19 08:59:20 +0000

    created sign out functionality

--- server.rb
+++ server.rb
@@ -50,3 +50,9 @@ post '/sessions' do
     erb :"sessions/new"
   end
 end
+
+delete '/sessions' do
+  flash[:notice] = "Good bye!"
+  session[:user_id] = nil
+  redirect to('/')
+end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,5 +1,8 @@
 require './lib/user'
 require 'spec_helper'
+require './spec/features/helpers/session'
+
+include SessionHelpers
 
 feature "User signs up" do
 
@@ -19,14 +22,6 @@ feature "User signs up" do
     expect{ sign_up }.to change(User, :count).by(1)
     expect{ sign_up }.to change(User, :count).by(0)
     expect(page).to have_content("This email is already taken")
-  end
-
-  def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = 'oranges!')
-    visit '/users/new'
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
   end
 
 end
@@ -53,11 +48,21 @@ feature "User signs in" do
     expect(page).not_to have_content("Welcome, test@test.com")
   end
 
-  def sign_in(email, password)
-    visit '/sessions/new'
-    fill_in 'email', :with => email
-    fill_in 'password', :with => password
-    click_button 'Sign in'
+end
+
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button "Sign out"
+    expect(page).to have_content("Good bye!")
+    expect(page).not_to have_content("Welcome, test@test.com")
   end
 
 end
