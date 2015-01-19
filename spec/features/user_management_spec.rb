c684b69705f26664a1eabafb97cbc908090dd376
kierangoodacre <k.goodacre@hotmail.co.uk>
2015-01-19 08:36:49 +0000

    created sign in process and passed tests

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -21,19 +21,43 @@ feature "User signs up" do
     expect(page).to have_content("This email is already taken")
   end
 
-  def sign_in(email, password)
-    visit '/sessions/new'
-    fill_in 'email', :with => email
-    fill_in 'password', :with => password
-    click_button 'Sign in'
-  end
-  
   def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = 'oranges!')
     visit '/users/new'
     fill_in :email, :with => email
     fill_in :password, :with => password
     fill_in :password_confirmation, :with => password_confirmation
     click_button "Sign up"
+  end
+
+end
+
+feature "User signs in" do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', :with => email
+    fill_in 'password', :with => password
+    click_button 'Sign in'
   end
 
 end
